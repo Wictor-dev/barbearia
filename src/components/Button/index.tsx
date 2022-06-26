c7d2b41274f3ebb3a0ce2fa57c81ee7405f8f365
Wictor-dev <wictorgabrielmourao@gmail.com>
2022-06-26 11:17:57 -0300

    feat: finish PerfilEmployer Screen

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -6,15 +6,20 @@ import { ButtonContainer } from "./ButtonStyles";
 type ButtonProps = TouchableOpacityProps & {
     title: string,
     border?: boolean,
-    size: string,
+    size: "small" | "medium" | "large",
     color?: string,
 }
 
 export function Button({ title, border, size = 'large', color = theme.colors.b01, ...rest} : ButtonProps){
-    const fontBtn = size === 'large' ? 24 : 12;
+    const sizes = { 
+        small: 12,
+        medium: 16,
+        large: 24
+    }
+
     return (
         <ButtonContainer size={size} border={border} color={color} {...rest}> 
-            <Text style={{fontSize: fontBtn, fontWeight: '600', color: theme.colors.g12}}>{title}</Text>
+            <Text style={{fontSize: sizes[size], fontWeight: '600', color: theme.colors.g12}}>{title}</Text>
             
         </ButtonContainer>
         
--- src/pages/PerfilEmployer/PerfilEmployerStyles.ts
+++ src/pages/PerfilEmployer/PerfilEmployerStyles.ts
@@ -11,11 +11,15 @@ export const Container = styled.View`
     align-self: flex-end;
     padding: 16px 20px;
     z-index: 1;
+    align-items: flex-end;
 `
 
 export const Title = styled.Text`
     font-size: 28px;
-    align-self: flex-end;
+    /* align-self: flex-end; */
+    margin-bottom: 92px;
+    color: ${theme.colors.b11};
+    font-weight: bold;
 `
 
 export const PerfilArea = styled(PerfilContainer)`
@@ -25,4 +29,19 @@ export const PerfilArea = styled(PerfilContainer)`
     top: 15%;
     margin-left: 24px;
     z-index: 5;
+`
+
+export const ServicesContainer = styled.View`
+    /* flex: 1; */
+    width: 100%;
+    align-items: flex-start;
+`
+
+export const ServiceContainer = styled.View`
+    width: 100%;
+    height: 52px;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 20px;
 `
